30379e1fc02365c8756f96e6efa2a922f19a6f26
Okunade sherif <o.d.alsherif@gmail.com>
2023-01-11 05:00:35 +0100

    deploy

--- src/Components/TestimonialCard.jsx
+++ src/Components/TestimonialCard.jsx
@@ -3,7 +3,7 @@ import indexStyles from '../styles/index.module.css'
 
 function TestimonialCard({img, name, testimonial}) {
   return (
-    <div className={`w-[282px] h-[400px] hover:bg-[#9BE150] 2xl:w-[20%] 2xl:h-[80%] bg-[#FFFFFF] flex flex-col relative xl:justify-between just px-8 mt-[20%] lg:mt-[7%] xl:mt-[2%]`} style={{boxShadow: '0px 16px 40px rgba(117, 142, 254, 0.25)'}}>
+    <div className={`w-[282px] h-[400px] hover:bg-[#9BE150] 2xl:w-[20%] 2xl:h-[80%] bg-[#FFFFFF] flex flex-col relative xl:justify-between px-8 mt-[20%] lg:mt-[7%] xl:mt-[2%] 3xl:h-[550px]`} style={{boxShadow: '0px 16px 40px rgba(117, 142, 254, 0.25)'}}>
       <div className='self-center w-[60%] -mt-[30%] h-[40%] '>
         <img src={img} className='w-full rounded-full' /> 
       </div>
